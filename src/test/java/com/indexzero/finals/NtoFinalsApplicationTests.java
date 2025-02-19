60c6f318d6ee6f0f98d15ba2240bdf7bdbcbf5de
IndexZero <l0rmin@yandex.ru>
2025-02-19 15:31:55 +0300

    Login unit test

--- src/test/java/com/indexzero/finals/NtoFinalsApplicationTests.java
+++ src/test/java/com/indexzero/finals/NtoFinalsApplicationTests.java
@@ -1,13 +1,32 @@
 package com.indexzero.finals;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class NtoFinalsApplicationTests {
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
-    void contextLoads() {
+    void userLogin() throws Exception {
+        this.mockMvc.perform(
+                post("/api/employee/login")
+                    .with(httpBasic("pivanov", "HelloWorld1234"))
+                )
+                .andDo(print())
+                .andExpect(status().isOk()
+        );
     }
 
 }
