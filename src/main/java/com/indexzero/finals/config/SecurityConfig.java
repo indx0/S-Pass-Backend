21d002bce5d396193edfcee9dd83462ff3acc3d6
IndexZero <l0rmin@yandex.ru>
2025-02-19 18:54:45 +0300

    Changed name in changesets and improved SecurityConfig so there's no warns

--- src/main/java/com/indexzero/finals/config/SecurityConfig.java
+++ src/main/java/com/indexzero/finals/config/SecurityConfig.java
@@ -4,7 +4,9 @@ import com.indexzero.finals.service.impl.UserDetailsServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -61,11 +63,11 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationProvider authenticationProvider(){
+    public AuthenticationManager authenticationManager(){
         DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
         provider.setUserDetailsService(userDetailsService);
         provider.setPasswordEncoder(passwordEncoder());
-        return provider;
+        return new ProviderManager(provider);
     }
 
     @Bean
